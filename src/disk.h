8badbba5470d88f9e512d8160bb226a4c31ffe3b
hedwig100 <shota.in26@gmail.com>
2024-07-07 19:09:58 +0900

    [add] add disk manager

---
+++ src/disk.h
@@ -0,0 +1,75 @@
+#ifndef DISK_H
+#define DISK_H
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "result.h"
+
+using namespace ::result;
+
+namespace disk {
+
+// ID of the block. This is is represented as a pair of `filename` and
+// `block_index`. The `block_index` represents the 0-indexed index of the
+// block in the file with the block size.
+class BlockID {
+  public:
+    BlockID(const std::string &filename, const int block_index);
+
+    // Returns the filename.
+    const std::string &Filename() const;
+
+    // Returns the block index.
+    const int BlockIndex() const;
+
+  private:
+    const std::string filename_;
+    const int block_index_;
+};
+
+// The unit of data exchanged with disk.
+class Block {
+  public:
+    Block();
+    Block(const int block_size, const char *content);
+
+    // Reads the byte with the `offset`.
+    ResultV<uint8_t> ReadByte(const int offset) const;
+
+    // Returns the content of the block.
+    const std::vector<uint8_t> &Content() const;
+
+  private:
+    std::vector<uint8_t> content_;
+};
+
+// Manages writes and reads to disk.
+class DiskManager {
+  public:
+    DiskManager(const std::string &directory_path, const int block_size);
+
+    // Reads the bytes of `block_id` into `block`.
+    Result Read(const BlockID &block_id, Block &block) const;
+
+    // Writes the bytes `block` to the place of `block_id`.
+    Result Write(const BlockID &block_id, const Block &block) const;
+
+    // Flushes the writes of `directory_path`/`filename` to the disk.
+    Result Flush(const std::string &filename) const;
+
+    // Allocates new blocks until the id of `block_id`.
+    // If file of `block_id.Filename()` does not exist, this function creates a
+    // new file and resize it to the `block_id.BlockIndex()`. If file of
+    // `block_id.Filename()` exists, resize it.
+    Result AllocateNewBlocks(const BlockID &block_id) const;
+
+  private:
+    const std::string directory_path_;
+    const int block_size_;
+};
+
+} // namespace disk
+
+#endif // DISK_H
---
+++ src/disk_test.cc
@@ -0,0 +1,182 @@
+#include "disk.h"
+#include <cstdio>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <gtest/gtest.h>
+#include <iostream>
+
+TEST(BlockID, ReturnsCorrectFilename) {
+    const disk::BlockID block_id("metadata.tbl", 0);
+    EXPECT_EQ(block_id.Filename(), "metadata.tbl");
+}
+
+TEST(BlockID, ReturnsCorrectBlockIndex) {
+    const disk::BlockID block_id("metatable.tbl", 0);
+    EXPECT_EQ(block_id.BlockIndex(), 0);
+}
+
+TEST(Block, InstanciationAndReadByte) {
+    char hello[] = "hello";
+    const disk::Block block(5, hello);
+    EXPECT_EQ(block.ReadByte(0).Get(), 'h');
+    EXPECT_EQ(block.ReadByte(1).Get(), 'e');
+    EXPECT_EQ(block.ReadByte(2).Get(), 'l');
+    EXPECT_EQ(block.ReadByte(3).Get(), 'l');
+    EXPECT_EQ(block.ReadByte(4).Get(), 'o');
+}
+
+TEST(Block, ReadByteWithOutsideIndex) {
+    char hello[] = "hello";
+    const disk::Block block(5, hello);
+    EXPECT_TRUE(block.ReadByte(5).IsError());
+    EXPECT_TRUE(block.ReadByte(6).IsError());
+}
+
+class TempFileTest : public ::testing::Test {
+  protected:
+    TempFileTest() : directory_path("parent/dir/"), filename("metadata.tbl") {
+        if (!std::filesystem::exists(directory_path)) {
+            std::filesystem::create_directories(directory_path);
+        }
+
+        std::ofstream file(directory_path + filename);
+        if (file.is_open()) {
+            file << "hello ";
+            file.close();
+        }
+    }
+
+    virtual ~TempFileTest() override {
+        remove((directory_path + filename).c_str());
+    }
+
+    const std::string directory_path;
+    const std::string filename;
+};
+
+class NonExistentFileTest : public ::testing::Test {
+  protected:
+    NonExistentFileTest()
+        : directory_path("non/existent/directory/"),
+          non_existent_filename("deleted.txt") {
+        if (std::filesystem::exists(directory_path + non_existent_filename)) {
+            remove((directory_path + non_existent_filename).c_str());
+        }
+    }
+
+    virtual ~NonExistentFileTest() override {
+        if (std::filesystem::exists(directory_path + non_existent_filename)) {
+            remove((directory_path + non_existent_filename).c_str());
+        }
+    }
+
+    const std::string directory_path;
+    const std::string non_existent_filename;
+};
+
+TEST_F(TempFileTest, DiskManagerCorrectlyReads) {
+    const disk::DiskManager disk_manager(directory_path, /*block_size=*/3);
+    disk::Block block;
+
+    EXPECT_TRUE(disk_manager.Read(disk::BlockID(filename, 0), block).IsOk());
+
+    // block must be "hel"
+    EXPECT_EQ(block.ReadByte(0).Get(), 'h');
+    EXPECT_EQ(block.ReadByte(1).Get(), 'e');
+    EXPECT_EQ(block.ReadByte(2).Get(), 'l');
+    EXPECT_TRUE(block.ReadByte(3).IsError());
+
+    EXPECT_TRUE(disk_manager.Read(disk::BlockID(filename, 1), block).IsOk());
+
+    // block must be "lo "
+    EXPECT_EQ(block.ReadByte(0).Get(), 'l');
+    EXPECT_EQ(block.ReadByte(1).Get(), 'o');
+    EXPECT_EQ(block.ReadByte(2).Get(), ' ');
+    EXPECT_TRUE(block.ReadByte(3).IsError());
+}
+
+TEST_F(NonExistentFileTest, DiskManagerReadFail) {
+    const disk::DiskManager disk_manager(directory_path,
+                                         /*block_size=*/3);
+    disk::Block block;
+
+    EXPECT_TRUE(
+        disk_manager.Read(disk::BlockID(non_existent_filename, 0), block)
+            .IsError());
+}
+
+TEST_F(TempFileTest, DiskManagerCorrectlyWrites) {
+    const disk::DiskManager disk_manager(directory_path, /*block_size=*/3);
+
+    char goodbye[] = "goodbye";
+    disk::Block block_write(3, goodbye), block_read;
+
+    EXPECT_TRUE(
+        disk_manager.Write(disk::BlockID(filename, 0), block_write).IsOk());
+    EXPECT_TRUE(
+        disk_manager.Read(disk::BlockID(filename, 0), block_read).IsOk());
+
+    // block_read must be "goo"
+    EXPECT_EQ(block_read.ReadByte(0).Get(), 'g');
+    EXPECT_EQ(block_read.ReadByte(1).Get(), 'o');
+    EXPECT_EQ(block_read.ReadByte(2).Get(), 'o');
+    EXPECT_TRUE(block_read.ReadByte(3).IsError());
+
+    EXPECT_TRUE(
+        disk_manager.Write(disk::BlockID(filename, 1), block_write).IsOk());
+    EXPECT_TRUE(
+        disk_manager.Read(disk::BlockID(filename, 1), block_read).IsOk());
+
+    // block must be "goo"
+    EXPECT_EQ(block_read.ReadByte(0).Get(), 'g');
+    EXPECT_EQ(block_read.ReadByte(1).Get(), 'o');
+    EXPECT_EQ(block_read.ReadByte(2).Get(), 'o');
+    EXPECT_TRUE(block_read.ReadByte(3).IsError());
+}
+
+TEST_F(NonExistentFileTest, DiskManagerWriteFail) {
+    const disk::DiskManager disk_manager(directory_path,
+                                         /*block_size=*/3);
+    disk::Block block;
+
+    EXPECT_TRUE(
+        disk_manager.Write(disk::BlockID(non_existent_filename, 0), block)
+            .IsError());
+}
+
+TEST_F(TempFileTest, DiskManagerFlushSucceeds) {
+    const disk::DiskManager disk_manager(directory_path, /*block_size=*/3);
+    EXPECT_TRUE(disk_manager.Flush(filename).IsOk());
+}
+
+TEST_F(NonExistentFileTest, DiskManagerFlushFails) {
+    const disk::DiskManager disk_manager(directory_path, /*block_size=*/3);
+    EXPECT_TRUE(disk_manager.Flush(non_existent_filename).IsError());
+}
+
+TEST_F(TempFileTest, DiskManagerAllocateNewFileSucceeds) {
+    const int block_size  = 3;
+    const int block_index = 10;
+
+    const disk::DiskManager disk_manager(directory_path, block_size);
+    EXPECT_TRUE(
+        disk_manager.AllocateNewBlocks(disk::BlockID(filename, block_index))
+            .IsOk());
+    EXPECT_EQ(std::filesystem::file_size(directory_path + filename),
+              block_index * block_size);
+}
+
+TEST_F(NonExistentFileTest, DiskManagerAllocateNewFileSucceedsWithoutFile) {
+    const int block_size  = 3;
+    const int block_index = 10;
+
+    const disk::DiskManager disk_manager(directory_path, block_size);
+    EXPECT_TRUE(disk_manager
+                    .AllocateNewBlocks(
+                        disk::BlockID(non_existent_filename, block_index))
+                    .IsOk());
+    EXPECT_EQ(
+        std::filesystem::file_size(directory_path + non_existent_filename),
+        block_index * block_size);
+}
