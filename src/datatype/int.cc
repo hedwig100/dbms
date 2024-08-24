44d8004f031b66ce54e31b380dac213d22dd95f1
hedwig100 <shota.in26@gmail.com>
2024-08-24 21:17:06 +0900

    [add] datatype

---
+++ src/datatype/int.cc
@@ -0,0 +1,24 @@
+#include "int.h"
+#include <cstring>
+
+namespace datatype {
+
+const static int kIntBytesize = 4;
+
+ResultV<int> ReadInt(const std::vector<uint8_t> &bytes, const int offset) {
+    if (offset < 0 || offset + kIntBytesize > bytes.size())
+        return Error("offset should be fit the size");
+    int read_value = 0;
+    std::memcpy(&read_value, &(bytes[offset]), kIntBytesize);
+    return Ok(read_value);
+}
+
+Result WriteInt(std::vector<uint8_t> &bytes, const int offset,
+                const int value) {
+    if (offset < 0 || offset + kIntBytesize > bytes.size())
+        return Error("offset should be fit the size");
+    std::memcpy(&(bytes[offset]), &value, kIntBytesize);
+    return Ok();
+}
+
+}
