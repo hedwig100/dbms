8aaa9271172322c6a15163be32e147603f4a9af8
hedwig100 <shota.in26@gmail.com>
2024-09-20 21:51:45 +0900

    [add] add test for log read

--- src/log_test.cc
+++ src/log_test.cc
@@ -121,3 +121,198 @@ TEST_F(LogFileExistentLogManager, FlushSuccess) {
     auto flush_result            = log_manager.Flush(lsn);
     EXPECT_TRUE(flush_result.IsOk());
 }
+
+FILE_EXISTENT_TEST(LogFileEmptyLogManager, "");
+
+TEST_F(LogFileEmptyLogManager, WriteAndReadLastLog) {
+    dblog::LogManager log_manager(/*log_filename=*/filename,
+                                  /*log_directory_name=*/directory_path,
+                                  /*block_size=*/32);
+    ASSERT_TRUE(log_manager.Init().IsOk());
+    std::vector<uint8_t> log_record_bytes = {
+        /*checksum*/ '\0',     '\0', '\0', '\0',
+        /*log body length*/ 4, '\0', '\0', '\0',
+        /*log body*/ 'a',      'b',  'c',  'd',
+        /*log body length*/ 4, '\0', '\0', '\0'};
+    std::vector<uint8_t> log_body_bytes = {
+        'a',
+        'b',
+        'c',
+        'd',
+    };
+
+    auto write_result = log_manager.WriteLog(log_record_bytes);
+    ASSERT_TRUE(write_result.IsOk()) << write_result.Error() << '\n';
+    ASSERT_TRUE(log_manager.Flush().IsOk());
+
+    auto last_log_iterator_result = log_manager.LastLog();
+    EXPECT_TRUE(last_log_iterator_result.IsOk())
+        << last_log_iterator_result.Error() << '\n';
+
+    auto last_log_iterator = last_log_iterator_result.Get();
+    auto log_body_result   = last_log_iterator.LogBody();
+    EXPECT_TRUE(log_body_result.IsOk()) << log_body_result.Error() << '\n';
+    EXPECT_EQ(log_body_result.Get(), log_body_bytes);
+}
+
+TEST_F(LogFileEmptyLogManager, WriteAndReadTooLongLastLog) {
+    dblog::LogManager log_manager(/*log_filename=*/filename,
+                                  /*log_directory_name=*/directory_path,
+                                  /*block_size=*/16);
+    ASSERT_TRUE(log_manager.Init().IsOk());
+    std::vector<uint8_t> log_record_bytes = {
+        '\0', '\0', '\0', '\0', 26,  '\0', '\0', '\0', 'a', 'b',
+        'c',  'd',  'e',  'f',  'g', 'h',  'i',  'j',  'k', 'l',
+        'm',  'n',  'o',  'p',  'q', 'r',  's',  't',  'u', 'v',
+        'w',  'x',  'y',  'z',  26,  '\0', '\0', '\0'};
+    std::vector<uint8_t> log_body_bytes = {
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    };
+
+    auto write_result = log_manager.WriteLog(log_record_bytes);
+    ASSERT_TRUE(write_result.IsOk()) << write_result.Error() << '\n';
+    ASSERT_TRUE(log_manager.Flush().IsOk());
+
+    auto last_log_iterator_result = log_manager.LastLog();
+    EXPECT_TRUE(last_log_iterator_result.IsOk())
+        << last_log_iterator_result.Error() << '\n';
+
+    auto last_log_iterator = last_log_iterator_result.Get();
+    auto log_body_result   = last_log_iterator.LogBody();
+    EXPECT_TRUE(log_body_result.IsOk()) << log_body_result.Error() << '\n';
+    EXPECT_EQ(log_body_result.Get(), log_body_bytes);
+}
+
+class LogFileInitialized : public ::testing::Test {
+  protected:
+    LogFileInitialized()
+        : directory_path("parent_dir/"), filename("filename"), block_size(16) {
+        if (!std::filesystem::exists(directory_path)) {
+            std::filesystem::create_directories(directory_path);
+        }
+        std::ofstream file(directory_path + filename);
+        file.close();
+
+        dblog::LogManager log_manager(filename, directory_path, block_size);
+        log_manager.Init();
+
+        std::vector<uint8_t> log_record_bytes1 = {
+            '\0', '\0', '\0', '\0', 26,  '\0', '\0', '\0', 'a', 'b',
+            'c',  'd',  'e',  'f',  'g', 'h',  'i',  'j',  'k', 'l',
+            'm',  'n',  'o',  'p',  'q', 'r',  's',  't',  'u', 'v',
+            'w',  'x',  'y',  'z',  26,  '\0', '\0', '\0'};
+        log_manager.WriteLog(log_record_bytes1);
+
+        std::vector<uint8_t> log_record_bytes2 = {'\0', '\0', '\0', '\0', 1,
+                                                  '\0', '\0', '\0', 'a',  1,
+                                                  '\0', '\0', '\0'};
+        log_manager.WriteLog(log_record_bytes2);
+
+        std::vector<uint8_t> log_record_bytes3 = {
+            '\0', '\0', '\0', '\0', 13,   '\0', '\0', '\0', 'a',
+            'b',  'c',  'd',  'e',  'f',  'g',  'h',  'i',  'j',
+            'k',  'l',  'm',  13,   '\0', '\0', '\0'};
+        log_manager.WriteLog(log_record_bytes3);
+
+        log_manager.Flush();
+    }
+
+    virtual ~LogFileInitialized() override {
+        std::filesystem::remove_all(directory_path);
+    }
+
+    const std::string directory_path;
+    const std::string filename;
+    const int block_size;
+};
+
+TEST_F(LogFileInitialized, LogManagerInitWhenLogExists) {
+    dblog::LogManager log_manager(/*log_filename=*/filename,
+                                  /*log_directory_name=*/directory_path,
+                                  /*block_size=*/block_size);
+
+    auto result = log_manager.Init();
+    EXPECT_TRUE(result.IsOk()) << result.Error() << '\n';
+
+    auto last_log_result = log_manager.LastLog();
+    EXPECT_TRUE(last_log_result.IsOk()) << last_log_result.Error() << '\n';
+    auto last_log = last_log_result.Get();
+
+    std::vector<uint8_t> expect_last_log = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                                            'h', 'i', 'j', 'k', 'l', 'm'};
+
+    auto last_log_body = last_log.LogBody();
+    EXPECT_TRUE(last_log_body.IsOk()) << last_log_body.Error() << '\n';
+    EXPECT_EQ(last_log_body.Get(), expect_last_log);
+}
+
+TEST_F(LogFileInitialized, LogIteratorPrevious) {
+    dblog::LogManager log_manager(/*log_filename=*/filename,
+                                  /*log_directory_name=*/directory_path,
+                                  /*block_size=*/block_size);
+
+    auto result = log_manager.Init();
+    ASSERT_TRUE(result.IsOk()) << result.Error() << '\n';
+
+    auto last_log_result = log_manager.LastLog();
+    ASSERT_TRUE(last_log_result.IsOk()) << last_log_result.Error() << '\n';
+    auto log = last_log_result.Get();
+
+    // log_record 2
+    result = log.Previous();
+    EXPECT_TRUE(result.IsOk()) << result.Error() << '\n';
+    auto log_body = log.LogBody();
+    EXPECT_TRUE(log_body.IsOk()) << log_body.Error() << '\n';
+    std::vector<uint8_t> expect_log = {'a'};
+    EXPECT_EQ(log_body.Get(), expect_log);
+
+    // log_record 1
+    result = log.Previous();
+    EXPECT_TRUE(result.IsOk()) << result.Error() << '\n';
+    log_body = log.LogBody();
+    EXPECT_TRUE(log_body.IsOk()) << log_body.Error() << '\n';
+    expect_log = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                  's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    EXPECT_EQ(log_body.Get(), expect_log);
+
+    // No log record
+    result = log.Previous();
+    EXPECT_TRUE(result.IsError());
+}
+
+TEST_F(LogFileInitialized, LogIteratorNext) {
+    dblog::LogManager log_manager(/*log_filename=*/filename,
+                                  /*log_directory_name=*/directory_path,
+                                  /*block_size=*/block_size);
+
+    auto result = log_manager.Init();
+    ASSERT_TRUE(result.IsOk()) << result.Error() << '\n';
+
+    auto last_log_result = log_manager.LastLog();
+    ASSERT_TRUE(last_log_result.IsOk()) << last_log_result.Error() << '\n';
+    auto log = last_log_result.Get();
+
+    result = log.Previous();
+    EXPECT_TRUE(result.IsOk()) << result.Error() << '\n';
+    result = log.Previous();
+    EXPECT_TRUE(result.IsOk()) << result.Error() << '\n';
+
+    // log record 2
+    result = log.Next();
+    EXPECT_TRUE(result.IsOk()) << result.Error() << '\n';
+    auto log_body = log.LogBody();
+    EXPECT_TRUE(log_body.IsOk()) << log_body.Error() << '\n';
+    std::vector<uint8_t> expect_log = {'a'};
+    EXPECT_EQ(log_body.Get(), expect_log);
+
+    // log record 3
+    result = log.Next();
+    EXPECT_TRUE(result.IsOk()) << result.Error() << '\n';
+    log_body = log.LogBody();
+    EXPECT_TRUE(log_body.IsOk()) << log_body.Error() << '\n';
+    expect_log = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                  'h', 'i', 'j', 'k', 'l', 'm'};
+    EXPECT_EQ(log_body.Get(), expect_log);
+}
