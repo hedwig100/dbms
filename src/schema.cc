9aa77e8ed9ba76f221b12d4192aabab40d49f7c3
hedwig100 <shota.in26@gmail.com>
2025-02-06 20:17:41 +0900

    [add] schema::Length(fieldname)

--- src/schema.cc
+++ src/schema.cc
@@ -2,13 +2,15 @@
 
 namespace schema {
 
+// The first byte is used for the used/unused flag.
 constexpr int kUsedFlagOffset = 0;
 constexpr int kUsedFlagLength = 1;
 
-Layout::Layout(const Schema &schema) : schema(schema) {
+Layout::Layout(const Schema &schema) {
     int offset = kUsedFlagLength;
     for (const auto &field : schema.Fields()) {
-        offsets[field.FieldName()] = offset;
+        field_lengths[field.FieldName()] = field.Length();
+        offsets[field.FieldName()]       = offset;
         offset += field.Length();
     }
     length = offset;
