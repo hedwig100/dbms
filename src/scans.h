d22deb9f0b3f056198654138b98d76c213d73e11
hedwig100 <shota.in26@gmail.com>
2025-04-05 18:00:20 +0900

    wip: [add] scans

---
+++ src/scans.h
@@ -0,0 +1,15 @@
+#ifndef _SCANS_H
+#define _SCANS_H
+
+#include "result.h"
+#include "scan.h"
+
+namespace scan {
+
+using namespace ::result;
+
+class SelectScan : public Scan {};
+
+} // namespace scan
+
+#endif // _SCANS_H
