e177fa999c140509f17aedc64fb3d93ed0950f27
hedwig100 <shota.in26@gmail.com>
2025-06-08 20:18:40 +0900

    [add] DataItemWithType

--- src/data/data_test.cc
+++ src/data/data_test.cc
@@ -1,4 +1,5 @@
 #include "data/data.h"
+#include "data/int.h"
 #include <gtest/gtest.h>
 
 TEST(Data, Equal) {
@@ -61,4 +62,17 @@ TEST(DataCopy, WriteSuccess) {
 
     EXPECT_TRUE(data::Write(item, bytes, 5, 4).IsOk());
     EXPECT_EQ(bytes, expect_bytes);
+}
+
+TEST(DataItemWithType, EqualSucess) {
+    data::DataItem item1 = data::Int(1234);
+    data::DataItem item2 = data::Int(1234);
+    data::DataItem item3 = data::Int(5678);
+
+    data::DataItemWithType item_with_type1(item1, data::kTypeInt);
+    data::DataItemWithType item_with_type2(item2, data::kTypeInt);
+    data::DataItemWithType item_with_type3(item3, data::kTypeInt);
+
+    EXPECT_TRUE(item_with_type1 == item_with_type2);
+    EXPECT_TRUE(item_with_type1 != item_with_type3);
 }
